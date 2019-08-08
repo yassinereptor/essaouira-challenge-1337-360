b14d743895ac324b399f45f638fb4efbb1a4116f
Yassine reptor <ouarzaziyassine@gmail.com>
2019-08-08 01:40:50 +0000

    6456156

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-360';
+
+export default class vr_view extends React.Component {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('vr_view', () => vr_view);
